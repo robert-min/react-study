6822dd405860d30f158fd4af77f703065ab5d063
robert-min <robertmin522@gmail.com>
2023-01-03 15:19:57 +0900

    react todo & bitcoin collect study

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,32 +1,32 @@
-// import Button from "./Button";
-// import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  function byFn() {
-    console.log("Destroy : ");
-  }
-  function hiFn() {
-    console.log("Created : ");
-    return byFn;
-  }
-  useEffect(hiFn, []);
-  return (
-    <h1>Hello</h1>
-  );
-};
-
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev)
-
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-    {showing ? <Hello />: null}
-    <button onClick={onClick}>{showing ? "Hide": "Show"}</button>
+      <h1>The coins {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
-};
+}
 
 export default App;
